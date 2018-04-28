2086f8acfa039f034c29493eea93db50776f15e5
Micz Flor <micz.flor@web.de>
2018-04-28 18:30:09 +0200

    social share inc

---
+++ htdocs/inc.socialshare.php
@@ -0,0 +1,22 @@
+<!-- Social Share -->
+<!-- Facebook OpenGraph developers.facebook.com/docs/reference/opengraph/ -->
+<meta property="og:type" content="website">
+<meta property="og:title" content="Phoniebox - die freie Musikbox für Kinder, Erwachsene und Bastler/innen">
+<meta property="og:description" content="Hörspiele, Musik, sogar YouTube, Webradio und Spotify spielt die Musikbox. Einfach die Phonie-Karte über die Box streichen und der Hörspaß beginnt. Die Komponenten sind schnell zusammengesteckt und die Software gibt es gratis im Netz. Über WLAN lässt sich die Phoniebox auch mit Handy, PC und Tablet steuern.">
+<meta property="og:url" content="http://phoniebox.de">
+<meta property="og:image" content="http://phoniebox.de/img/Phoniebox-Social-Share-de.jpg">
+<!--meta property="og:site_name" content="Phoniebox" /-->
+
+<!-- Twitter domain approval dev.twitter.com/docs/cards/validation/validator -->
+<!-- For twitter, content is usually summary_large_image as shown -->
+<meta name="twitter:card" content=" summary_large_image">
+<meta name="twitter:title" content=" Phoniebox - die freie Musikbox für Kinder, Erwachsene und Bastler/innen">
+<meta name="twitter:description" content=" Hörspiele, Musik, sogar YouTube, Webradio und Spotify spielt die Musikbox. Einfach die Phonie-Karte über die Box streichen und der Hörspaß beginnt. Die Komponenten sind schnell zusammengesteckt und die Software gibt es gratis im Netz. Über WLAN lässt sich die Phoniebox auch mit Handy, PC und Tablet steuern.">
+<meta name="twitter:url" content="http://phoniebox.de">
+<meta name="twitter:image" content="http://phoniebox.de/img/Phoniebox-Social-Share-de.jpg">
+<meta name="twitter:creator" content="@miczflor">
+
+<!-- Shema.org microdata -->
+<meta itemprop="name" content="Phoniebox - die freie Musikbox für Kinder, Erwachsene und Bastler/innen">
+<meta itemprop="description" content="Hörspiele, Musik, sogar YouTube, Webradio und Spotify spielt die Musikbox. Einfach die Phonie-Karte über die Box streichen und der Hörspaß beginnt. Die Komponenten sind schnell zusammengesteckt und die Software gibt es gratis im Netz. Über WLAN lässt sich die Phoniebox auch mit Handy, PC und Tablet steuern.">
+<meta itemprop="image" content="http://phoniebox.de/img/Phoniebox-Social-Share-de.jpg">
